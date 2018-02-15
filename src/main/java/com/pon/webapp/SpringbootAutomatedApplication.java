68d160beecb5190976a9c847b779b4ef798c88a1
Nathapon <pon_engine@hotmail.com>
2018-02-15 16:32:50 +0700

    first commit

---
+++ src/main/java/com/pon/webapp/SpringbootAutomatedApplication.java
@@ -0,0 +1,12 @@
+package com.pon.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutomatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAutomatedApplication.class, args);
+	}
+}
